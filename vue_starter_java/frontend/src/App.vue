0d895cbed6956fa61a50d7e4c81067dece8e4e01
Andrej Goreta <andrej.goreta@gmail.com>
2019-08-05 17:28:33 -0400

    Vue Home Page TE-Gram

--- vue_starter_java/frontend/src/App.vue
+++ vue_starter_java/frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>
+      <router-link to="/"><h1>TE-Gram</h1></router-link>
     </div>
     <router-view/>
   </div>
--- vue_starter_java/frontend/src/views/Home.vue
+++ vue_starter_java/frontend/src/views/Home.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="home">
-    <h1>TE-Gram</h1>
+    <div class="post">
     <h4>Title for post</h4>
-    <img alt='img'>
+    <img src ="https://course_report_production.s3.amazonaws.com/rich/rich_files/rich_files/4763/original/jamie-justin-brian-tech-elevator-veterans.jpg" 
+          alt='img'>
     <p>08/05/2019 10:55am</p>
-    
+    </div>
   </div>
 </template>
 
@@ -28,3 +29,112 @@ export default {
 }
  
 </script>
+
+<style>
+
+a {
+  text-decoration: none;
+  
+}
+
+h1 {
+  color: blue;
+  text-align: center;
+  
+}
+
+h4 {
+  color:green;
+  text-align: left;
+  
+}
+
+body > div#app > div#nav{
+  text-align: center;
+  
+}
+
+div.home > div.post > p{
+  text-align: left;
+  
+}
+
+div.home > div.post{
+  
+  border: 5px solid black;
+  
+  
+}
+
+img {
+  align-content: center;
+  
+  
+}
+
+@media only screen and (max-width: 600px){
+  img {
+    object-fit: cover;
+    width: 100%;
+   
+  }
+}
+
+@media only screen and (min-width: 600px){
+  img {
+    object-fit: cover;
+    width: 100%;
+  }
+}
+
+@media only screen and (min-width: 768px){
+  img {
+    object-fit: cover;
+    width: 100%;
+  }
+
+  h4{
+    font-size: 1.5rem;
+    
+  }
+
+  p{
+    font-size: 1rem;
+  }
+}
+
+@media only screen and (min-width: 992px){
+  img {
+    object-fit: cover;
+    width: 100%;
+  }
+
+  h4{
+    font-size: 2rem;
+  }
+
+  p{
+    font-size: 1.5rem;
+  }
+
+@media only screen and (min-width: 1200px){
+  img {
+    
+    object-fit: cover;
+    width: 100%;
+    
+  }
+
+  div.home {
+    display: flex;
+    justify-content: space-around;
+  }
+
+  div.home > div.post{
+    width: 1000px;
+  }
+}
+}
+
+
+</style>
