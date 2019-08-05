049010abd5d8b40494060b0b297fe99673a011d3
Joe Curran <josephlcurran@gmail.com>
2019-08-05 12:47:52 -0400

    made a db,pushing to pull

--- vue_starter_java/frontend/src/views/Home.vue
+++ vue_starter_java/frontend/src/views/Home.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+    <h1>TE-Gram</h1>
+    <h4>Title for post</h4>
+    <img alt='img'>
+    <p>08/05/2019 10:55am</p>
+    
   </div>
 </template>
 
 <script>
+import moment from 'moment'
 export default {
-  name: 'home'
+  name: 'home',
+  
+ data() {
+   return {
+      dating: dating.filter('formatDate', function(value) {
+      if (value) {
+      return moment(String(value)).format('MM/DD/YYYY hh:mm')
+    }
+})
+   }
+ },
+ methods: {
+
+ } 
 }
+ 
 </script>
